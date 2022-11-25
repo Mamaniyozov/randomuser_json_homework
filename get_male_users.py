45464b9ef9efb7850b81393caae67f3381b7723e
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2022-11-25 09:19:45 +0500

    4- misol

--- get_male_users.py
+++ get_male_users.py
@@ -1,3 +1,4 @@
+from from_json import read_json
 def get_male_users(data:dict)->list:
     """Gets all male users from the data
     Args:
@@ -6,3 +7,8 @@ def get_male_users(data:dict)->list:
         list: A list of users
     """
     pass
+    users = data["users"]
+    return list(users)
+
+data = read_json("users.json")
+print(get_male_users(data))
